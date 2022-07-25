dcd91a606dec036317190ee4f43bdfd98ae37dd5
yixxas <limbangjin0@gmail.com>
2022-07-25 17:12:33 +0800

    add rinkeby testnet config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const dotenv = require('dotenv');
+dotenv.config();
+
 const config: HardhatUserConfig = {
+  networks: {
+      hardhat: {},
+      rinkeby: {
+          url: "https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
+          accounts: [`${process.env.KEY}`]
+      }
+  },
   solidity: {
     compilers: [
         {
